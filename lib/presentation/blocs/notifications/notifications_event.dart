054b3b41f89ce3531c77c4185c09641509af2021
AntonyFloresPaz <developer4@traxpark.us>
2025-02-19 11:29:05 -0500

    session 20 del curso de flutter

---
+++ lib/presentation/blocs/notifications/notifications_event.dart
@@ -0,0 +1,16 @@
+part of 'notifications_bloc.dart';
+
+abstract class NotificationsEvent {
+  const NotificationsEvent();
+}
+
+class NotificationStatusChanged extends NotificationsEvent {
+  final AuthorizationStatus status;
+
+  const NotificationStatusChanged(this.status);
+}
+
+class NotificationReceived extends NotificationsEvent {
+  final PushMessage notification;
+  NotificationReceived(this.notification);
+}
